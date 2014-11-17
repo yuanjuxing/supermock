3115d4401f1b7434ee946331236edbb50509236e
yuanjuxing <yuanjuxing@126.com>
2014-11-17 20:28:48 +0800

    ff

--- supermock-server/src/main/java/cn/pptest/ppmock/RequestHandler.java
+++ supermock-server/src/main/java/cn/pptest/ppmock/RequestHandler.java
@@ -4,9 +4,9 @@ public interface RequestHandler {
 	
 	public static final String HANDLER_CLASS_KEY = "RequestHandlerClass";
 
-	Response handle(Request request);
+	Response handle(Object msg);
 	
-    Response proxy(Request request);
+    Response proxy(Object msg);
     
-    Response stub(Request request);
+    Response stub(Object msg);
 }
